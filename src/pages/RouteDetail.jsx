f748f878a9847059c3f0f737938108a83fc13d8e
Julia Schwärzer <suedtirolmithund@gmail.com>
2026-02-21 16:23:25 +0000

    File changes

--- src/pages/RouteDetail.jsx
+++ src/pages/RouteDetail.jsx
@@ -299,9 +299,8 @@ export default function RouteDetail() {
           </Button>
         </Link>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-          {/* Main Content */}
-          <div className="lg:col-span-2 space-y-6">
+        <div className="grid grid-cols-1 gap-6">
+          <div className="space-y-6">
             {/* Header */}
             <motion.div
               initial={{ opacity: 0, y: 20 }}
