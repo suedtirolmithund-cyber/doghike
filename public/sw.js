679f0801ca2687ef54d4147e65fded0b28e12ba5
suedtirolmithund-cyber <suedtirolmithund@gmail.com>
2026-04-24 21:35:01 +0200

    Navigate notifications to target page

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,14 @@ self.addEventListener('notificationclick', (event) => {
   event.waitUntil(
     clients.matchAll({ type: 'window', includeUncontrolled: true }).then((list) => {
       for (const client of list) {
-        if ('focus' in client) return client.focus();
+        if ('focus' in client) {
+          return client.focus().then((focusedClient) => {
+            if ('navigate' in focusedClient) {
+              return focusedClient.navigate(url);
+            }
+            return focusedClient;
+          });
+        }
       }
       if (clients.openWindow) return clients.openWindow(url);
     })
