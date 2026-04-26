6b04b0eb7ba3aa3c7a4066e00eb4dee5a5d9cb45
suedtirolmithund-cyber <suedtirolmithund@gmail.com>
2026-04-26 19:55:05 +0200

    Keep onboarding continue button visible

--- src/components/GuestWelcomeScreen.jsx
+++ src/components/GuestWelcomeScreen.jsx
@@ -462,8 +462,7 @@ function OnboardingScreen({ onContinue }) {
           type="button"
           onClick={onContinue}
           aria-label="Weiter"
-          className="absolute left-[161px] top-[751px] grid h-[54px] w-[52px] place-items-center rounded-full bg-[#BE8C70]/80 text-white"
-          style={{ mixBlendMode: "plus-lighter" }}
+          className="absolute bottom-[7px] left-1/2 z-20 grid h-[54px] w-[52px] -translate-x-1/2 place-items-center rounded-full bg-[#BE8C70]/90 text-white shadow-[0_10px_28px_rgba(0,0,0,0.28)] ring-1 ring-white/35"
         >
           <ArrowRight className="h-[31px] w-[31px] stroke-[2.4]" />
         </button>
