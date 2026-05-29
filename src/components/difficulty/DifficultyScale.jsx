e62b213e43fdd2188aa2cec8c7286fee91655fdf
suedtirolmithund-cyber <suedtirolmithund@gmail.com>
2026-05-29 21:55:56 +0200

    Unify difficulty chip colors

--- src/components/difficulty/DifficultyScale.jsx
+++ src/components/difficulty/DifficultyScale.jsx
@@ -1,20 +1,22 @@
 import { TOUR_ICONS, getDifficultyLabel, getDifficultyLevel } from "@/lib/difficultyConfig";
 import { cn } from "@/lib/utils";
 
+const SHARED_DIFFICULTY_CHIP_STYLE = {
+  chipClass: "border-[#F9C030]/80 bg-[#FFF8F0] text-[#7C3020]",
+  activeBar: "bg-[#A8003C]",
+  inactiveBar: "bg-[#F3D9C9]",
+};
+
 const DIFFICULTY_TYPE_META = {
   human: {
     icon: TOUR_ICONS.human,
     label: "Mensch",
-    chipClass: "border-[#D4547A]/60 bg-[#FFF3F7] text-[#7C3020]",
-    activeBar: "bg-[#A8003C]",
-    inactiveBar: "bg-[#F2D3DD]",
+    ...SHARED_DIFFICULTY_CHIP_STYLE,
   },
   dog: {
     icon: TOUR_ICONS.dog,
     label: "Hund",
-    chipClass: "border-[#F9C030] bg-[#FFF8E0] text-[#7C3020]",
-    activeBar: "bg-[#C07820]",
-    inactiveBar: "bg-[#F4DFA8]",
+    ...SHARED_DIFFICULTY_CHIP_STYLE,
   },
 };
 
