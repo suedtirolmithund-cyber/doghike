718e406ed44c6463c532fe24a9366ef411058bad
suedtirolmithund-cyber <suedtirolmithund@gmail.com>
2026-06-09 16:05:00 +0200

    Clarify premium management action

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -260,7 +260,7 @@ export default function Premium() {
                   ) : (
                     <Settings className="mr-2 h-4 w-4" />
                   )}
-                  Abo verwalten
+                  Premium verwalten / kündigen
                 </Button>
               )}
             </div>
